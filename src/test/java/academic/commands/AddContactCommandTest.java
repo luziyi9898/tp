e2ecae0ec0b1b0841e6463589a8b8ac8cb61d4c1
luziyi9898 <luziyi9898@gmail.com>
2020-10-14 23:53:43 +0800

    Tried implementing JUnits test.

---
+++ src/test/java/academic/commands/AddContactCommandTest.java
@@ -0,0 +1,19 @@
+package academic.commands;
+
+import academic.Person;
+import academic.PersonBook;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AddContactCommandTest {
+    private static final ArrayList<Person> listOfPerson = new ArrayList<>();
+
+
+
+
+
+
+}
